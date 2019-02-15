44c6cfa1e0e14d78b3488870f99e80d8eeece9a7
Luana Ferreira <44387993+ferreiraluana@users.noreply.github.com>
2019-02-15 17:07:18 -0300

    Create Mozilla_spring_camp.c

---
+++ Mozilla_spring_camp.c
@@ -0,0 +1,94 @@
+/*STACK WITH LIST*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node node;
+
+struct node 
+{
+	int item;
+	struct node *previous;
+};
+
+typedef struct stack stack;
+
+struct stack
+{
+	node *top;
+};
+
+stack *create_stack()
+{
+	stack *new_stack = (stack*) malloc (sizeof(stack));
+	new_stack->top = NULL;
+	return new_stack;
+}
+
+int is_empty(stack *stack)
+{
+	return (stack->top == NULL);
+}
+
+void push(stack *stack, int item)
+{
+	node *new_top = (node*) malloc (sizeof(node));
+	new_top->item = item;
+	new_top->previous = stack->top;
+	stack->top = new_top;
+}
+
+void pop(stack *stack)
+{
+	node *current = (node*) malloc (sizeof(node));
+	if(is_empty(stack))
+		return 0;//printf("Stack Underflow\n");
+	else 
+	{
+		current = stack->top->previous;
+		stack->top->previous = NULL;
+		stack->top = current;
+	}	
+}
+
+int peek(stack *stack)
+{
+	if(is_empty(stack))
+		return 0;//printf("Stack Underflow\n");
+	else 
+		return stack->top->item;
+}
+
+void main()
+{
+	stack *stack_back = create_stack();
+	stack *stack_forward = create_stack();	
+	char item[10];
+	int b = 0;
+	int f = 0;
+	int enter = 0;
+	while(scanf("%s", item) != EOF)
+	{
+		if(!strcmp(item, "ENTER"))
+		{	
+			if(enter == 0)
+				enter = 1;
+			else
+				push(stack_back, ++b);	
+		}
+		else if(!strcmp(item, "BACK"))
+		{
+			pop(stack_back);
+			push(stack_forward, ++f);
+		}
+		else if(!strcmp(item, "FORWARD"))
+		{	
+			pop(stack_forward);
+			push(stack_back, ++b);
+		}	 
+	}
+	printf("BACK: %d\n", peek(stack_back));
+	printf("FORWARD: %d\n", peek(stack_forward));
+	free(stack_back);
+	free(stack_forward);
+}
